0cade24fd3a426f11474f2d4c18ac1110bb28866
SemNik <142649558+SemNik88@users.noreply.github.com>
2024-02-05 19:04:05 +0300

    Update CreditCardTest.java

--- src/test/java/ru/netology/CreditCardTest.java
+++ src/test/java/ru/netology/CreditCardTest.java
@@ -1,85 +1,116 @@
 package ru.netology;
 
-package ru.netology.SeleniumTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogType;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.openqa.selenium.By.cssSelector;
 
-class SeleniumTest {
-    WebDriver driver;
+public class CallbackTestWebDriverManager {
+    private WebDriver driver;
 
     @BeforeAll
-    static void setupClass() {
-
+    static void setUpAll() {
+//  библиотека webdriver manager автоматически определяет ОС и версию браузера, скачивает и устанавливает подходящий файл драйвера
         WebDriverManager.chromedriver().setup();
-
     }
 
     @BeforeEach
     void setUp() {
+//  запуск браузера в режиме headless - отключаем графический интерфейс
         ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-
         driver.get("http://localhost:9999");
     }
 
     @AfterEach
+//  завершение работы браузера
     void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldSendForm() {
-        //driver.get("http://localhost:9999");
-
-        driver.findElement( By.cssSelector("[data-test-id=name] input")).sendKeys("Гусев Иван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79164535391");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button__text")).click();
-        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-        assertEquals(expected, actual);
+    public void shouldSent() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector("[data-test-id=order-success]")).isEnabled());
     }
 
     @Test
-    void shouldSendFormNoName() {
-        //driver.get("http://localhost:9999");
-
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79164535391");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button__text")).click();
-        String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElements(By.className("input__sub")).get(0).getText().trim();
-        assertEquals(expected, actual);
+    public void shouldWarnIfAllFieldsEmpty() {
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
     }
 
     @Test
-    void shouldSendFormNoPhone() {
-        //driver.get("http://localhost:9999");
-
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Гусев Иван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("button__text")).click();
-        String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElements(By.className("input__sub")).get(1).getText().trim();
-        assertEquals(expected, actual);
+    public void shouldWarnIfNameFieldsEmpty() {
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
     }
 
+    @Test
+    public void shouldWarnIfTelephoneFieldsEmpty() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
 
+    @Test
+    public void shouldWarnIfCheckboxFieldsEmpty() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
 
+    @Test
+    public void shouldWarnIfBadName() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("kek lol");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+77777777777");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldWarnIfBadTelephone1() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+12345");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldWarnIfBadTelephone2() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+123456789012");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
+
+    @Test
+    public void shouldWarnIfBadTelephone3() {
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Денис Дорошенко");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+abcdefghijk");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector(".form-field .button__content")).click();
+        assertTrue(driver.findElement(cssSelector(".input_invalid")).isEnabled());
+    }
 }
